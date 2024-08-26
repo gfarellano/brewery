4c3f9b30855d4c758c3d814ccec327403ace3215
gfarellano <gfarrellano673@gmail.com>
2024-08-26 11:40:18 -0300

    comitie2mundis

--- src/beers/beers.service.ts
+++ src/beers/beers.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class BeersService {
     getHello(): string {
-      return 'Hello world!';
+      return 'Hola mundis!';
     }
 }
